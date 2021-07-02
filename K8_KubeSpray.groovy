3944695cd1282f1071889313a6c12966300449b5
escanoru <yitzo@yandex.com>
2021-07-02 01:04:01 -0600

    $

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -82,7 +82,7 @@ pipeline {
         stage('Write Inventory file') {
            steps {
                sh '''
-               echo "${params.inventory_conf} > "${WORKSPACE}/roles/inventory.yaml"
+               echo "${params.inventory_conf}" > "${WORKSPACE}/roles/inventory.yaml"
                '''
             //    script {
             //        writeFile(file: "${WORKSPACE}/roles/inventory.yaml", text: "${params.inventory_conf}", encoding: "UTF-8")
