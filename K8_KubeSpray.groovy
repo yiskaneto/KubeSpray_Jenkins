518ea5ffc42fece350e489c5a7f2a8fd063314f1
escanoru <yitzo@yandex.com>
2021-07-03 21:48:20 -0600

    sd

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -96,13 +96,11 @@ pipeline {
         stage('Creating Inventory file') {
 			steps {
                 // echo -e "[all]\\n\\n[kube_control_plane]\\n\\n[etcd]\\n\\n[kube_node]\\n\\n[calico_rr]\\n\\n[k8s_cluster:children]\\nkube_control_plane\\nkube_node\\ncalico_rr" > ${WORKSPACE}/inventory.ini
-                
 				sh '''
-                do echo ${kube_control_plane} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
-                cat ${WORKSPACE}/inventory.ini
-                do echo ${etcd} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
-                do echo ${kube_node} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
-                do echo ${calico_rr} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done		
+                echo ${kube_control_plane} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
+                echo ${etcd} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
+                echo ${kube_node} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
+                echo ${calico_rr} | sed \'s/,/\\n/g\' | while read line ; sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done		
                 cat ${WORKSPACE}/inventory.ini		
 				'''
 			}
