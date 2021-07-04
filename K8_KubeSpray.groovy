db6d0e2c6cdef10cdf490c4d63a200d4574cf6c0
escanoru <yitzo@yandex.com>
2021-07-04 16:58:41 -0600

    '

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -138,7 +138,7 @@ pipeline {
                     colorized: true,
                     become: true,
                     becomeUser: "root",
-                    extras: '-v --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile=~/.ssh/id_rsa"',
+                    extras: '-v --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"',
                     extraVars: [
                         proxy_addr: "${params.proxy_addr}",
                         no_proxy_addr: "${params.no_proxy_addr}",
