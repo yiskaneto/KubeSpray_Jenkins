0ac7096681a44bca9a0dede7dc260017591e6bc6
escanoru <yitzo@yandex.com>
2021-07-03 21:01:11 -0600

    sd

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -62,7 +62,7 @@ pipeline {
         description: '<h5>e.g http://my_proxy.com:8080</h5>'
         )
         string(
-            name: 'Master', 
+            name: 'Mkube_control_planeaster', 
             description: 'List of kube control planes IPs separated by comas"'
         )
         choice(
@@ -80,8 +80,8 @@ pipeline {
         stage('Creating Inventory file') {
 			steps {
 				sh '''
-				echo -e "[all]\\n\\n[kube_control_plane]\\n\\n[etcd]\\n\\n[kube_node]\\n\\n[calico_rr]\\n\\n[k8s_cluster:children]\\nkube_control_plane\\nkube_node\\ncalico_rr" > ${WORKSPACE}/inventory.ini ;
-                echo ${Master} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
+				echo -e "[all]\\n\\n[kube_control_plane]\\n\\n[etcd]\\n\\n[kube_node]\\n\\n[calico_rr]\\n\\n[k8s_cluster:children]\\nkube_control_plane\\nkube_node\\ncalico_rr" > ${WORKSPACE}/inventory.ini
+                echo ${kube_control_plane} | sed \'s/,/\\n/g\' | while read line ; do sed -i \'/\\[all\\]/a \\\'"${line}"\'\' ${WORKSPACE}/inventory.ini ; done
                 cat ${WORKSPACE}/inventory.ini
 				
 				'''
