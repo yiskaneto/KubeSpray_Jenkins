ff5e0c23ca10c20d7c80c775b335ddf05c91c505
escanoru <yitzo@yandex.com>
2021-07-04 19:19:21 -0600

    --flush-cache

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -152,7 +152,7 @@ pipeline {
                     colorized: true,
                     // become: true,
                     // becomeUser: "root",
-                    extras: '-v --become --become-user=root --ssh-extra-args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile=~/.ssh/id_rsa"',
+                    extras: '-v --become --become-user=root --flush-cache --ssh-extra-args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile=~/.ssh/id_rsa"',
                     extraVars: [
                         proxy_addr: "${params.proxy_addr}",
                         no_proxy_addr: "${params.no_proxy_addr}",
