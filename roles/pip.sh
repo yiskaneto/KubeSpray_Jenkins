3d7712fc8894d6b7c32d871a89c4f31ea8ace2a4
escanoru <yitzo@yandex.com>
2021-07-07 10:06:22 -0600

    echo -e "\n\n\n\n"

--- roles/pip.sh
+++ roles/pip.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
-echo "Starting python3 -m venv venv \n\n\n\n"
-python3 -m venv venv
+echo "Starting python3 -m venv venv"
+python3 -m venv venv ; echo -e "\n\n\n\n"
 
-echo "Starting source venv/bin/activate \n\n\n\n"
-source venv/bin/activate
+echo "Starting source venv/bin/activate"
+source venv/bin/activate ; echo -e "\n\n\n\n"
 
-echo -e "cd pwd \n\n\n\n"
-pwd
+echo -e "cd pwd"
+pwd ; echo -e "\n\n\n\n"
 
-echo "Running /usr/local/bin/pip3 install -r requirements.txt \n\n\n\n"
-/usr/local/bin/pip3 install -r requirements.txt
+echo "Running /usr/local/bin/pip3 install -r requirements.txt"
+/usr/local/bin/pip3 install -r requirements.txt ; echo -e "\n\n\n\n"
