6f9f976ec73790e414d28190e2df8e00c63839a1
escanoru <yitzo@yandex.com>
2021-07-07 10:25:25 -0600

    echo -e "Verifying current directory"

--- roles/pip.sh
+++ roles/pip.sh
@@ -6,13 +6,13 @@ python3 -m venv venv ; echo -e "\n\n"
 echo "Starting source venv/bin/activate"
 source venv/bin/activate ; echo -e "\n\n"
 
-echo -e "cd pwd"
+echo -e "Verifying current directory"
 pwd ; echo -e "\n"
 
-echo "Running /usr/local/bin/pip3 install -r requirements.txt\n"
+echo -e "Running /usr/local/bin/pip3 install -r requirements.txt\n"
 /usr/local/bin/pip3 install -r requirements.txt ; echo -e "\n\n"
 
-echo -e "cd pwd"
+echo -e "Verifying current directory"
 pwd ; echo -e "\n"
 
 echo "Deactivating venv"
