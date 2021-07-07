e861662bcc393c89987c2a0a7255853af1bbb091
escanoru <yitzo@yandex.com>
2021-07-07 14:41:23 -0600

    retry(8)

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -215,12 +215,16 @@ pipeline {
                 // deactivate ; echo -e "\n"s
                 
                 // Trying to use ansiblePlaybook method instead of the method above
-                ansiblePlaybook(
-                    playbook: "${env.WORKSPACE}/roles/tmp/kubespray/cluster.yml",
-                    inventory: "${WORKSPACE}/inventory.ini",
-                    colorized: true,
-                    extras: '-u root --become --become-user=root --flush-cache --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" -vv'
+                retry(8) {
+                    ansiblePlaybook(
+                        playbook: "${env.WORKSPACE}/roles/tmp/kubespray/cluster.yml",
+                        inventory: "${WORKSPACE}/inventory.ini",
+                        colorized: true,
+                        extras: '-u root --become --become-user=root --flush-cache --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" -vv'
+                    )    
                 )
+                    
+                }
             }
         }
     }
