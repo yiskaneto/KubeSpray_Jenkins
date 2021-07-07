b5a2d0994d25028a19cf129e4b6c51df9aad003b
escanoru <yitzo@yandex.com>
2021-07-07 10:01:48 -0600

    python3 -m venv venv

--- roles/pip.sh
+++ roles/pip.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
-echo "Starting pipenv shell"
-/usr/local/bin/pipenv shell
+echo "Starting python3 -m venv venv"
+python3 -m venv venv
+source venv/bin/activate
 
 echo "Running /usr/local/bin/pip3 install -r requirements.txt"
 /usr/local/bin/pip3 install -r requirements.txt
