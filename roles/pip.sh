ee870190b604bc402bfa9ca2b082854bc801b266
escanoru <yitzo@yandex.com>
2021-07-07 10:21:28 -0600

    echo "Deactive venv" deactivate ; echo -e "\n\n"

--- roles/pip.sh
+++ roles/pip.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/env bash
 
-echo "Starting python3 -m venv venv"
-python3 -m venv venv ; echo -e "\n\n\n\n"
+echo "\n\n\nStarting python3 -m venv venv"
+python3 -m venv venv ; echo -e "\n\n"
 
 echo "Starting source venv/bin/activate"
-source venv/bin/activate ; echo -e "\n\n\n\n"
+source venv/bin/activate ; echo -e "\n\n"
 
 echo -e "cd pwd"
-pwd ; echo -e "\n\n\n\n"
+pwd ; echo -e "\n\n"
 
 echo "Running /usr/local/bin/pip3 install -r requirements.txt"
-/usr/local/bin/pip3 install -r requirements.txt ; echo -e "\n\n\n\n"
+/usr/local/bin/pip3 install -r requirements.txt ; echo -e "\n\n"
+
+echo "Deactive venv"
+deactivate ; echo -e "\n\n"
