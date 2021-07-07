9aebf83730dd74077dd43b690fe1182ae4163877
escanoru <yitzo@yandex.com>
2021-07-07 09:55:28 -0600

    /usr/local/bin/pip3 install -r requirements.txt

--- roles/pip.sh
+++ roles/pip.sh
@@ -1,2 +1,7 @@
 #! /usr/bin/env bash
+
+echo "Starting pipenv shell"
+/usr/local/bin/pipenv shell
+
+echo "Running /usr/local/bin/pip3 install -r requirements.txt"
 /usr/local/bin/pip3 install -r requirements.txt
