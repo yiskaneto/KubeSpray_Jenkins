0f73f013eac24eb2af457e4fbba87017f5407182
escanoru <yitzo@yandex.com>
2021-07-15 09:44:20 -0600

    inventory_sample

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -6,7 +6,7 @@ def setDescription() {
 setDescription()
 
 
-def inventory_sample = '''# ## Configure 'ip' variable to bind kubernetes services on a
+inventory_sample = '''# ## Configure 'ip' variable to bind kubernetes services on a
 # ## different ip than the default iface
 # ## We should set etcd_member_name for etcd cluster. The node that is not a etcd member do not need to set the value, or can set the empty string value.
 [all]
@@ -42,7 +42,8 @@ def inventory_sample = '''# ## Configure 'ip' variable to bind kubernetes servic
 
 [k8s_cluster:children]
 kube_control_plane
-kube_node'''
+kube_node
+calico_rr'''
 
 pipeline {
 	agent { label 'ansible' }
