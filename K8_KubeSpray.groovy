4707c64c343fe74408adb3d2f19e85a43e6b24bc
escanoru <yitzo@yandex.com>
2021-07-17 17:35:16 -0600

    akip

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -267,58 +267,6 @@ pipeline {
                         apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}"
                     ]
                 )
-
-                // script {
-                //     try {
-                //         ansiblePlaybook(
-                //             playbook: "${env.WORKSPACE}/roles/tmp/kubespray/reset.yml",
-                //             inventory: "${env.WORKSPACE}/inventory.ini",
-                //             forks: 16,
-                //             colorized: true,
-                //             become: true,
-                //             becomeUser: "root",
-                //             extras: '-u ${user} --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --flush-cache -v',
-                //             extraVars: [
-                //                 http_proxy: "${params.http_proxy}",
-                //                 https_proxy: "${params.https_proxy}",
-                //                 no_proxy: "${params.no_proxy}",
-                //                 reset_confirmation: "yes"
-                //             ]
-                //         )
-
-                //         ansiblePlaybook(
-                //             playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
-                //             inventory: "${env.WORKSPACE}/inventory.ini",
-                //             forks: 16,
-                //             colorized: true,
-                //             extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-                //             extraVars: [
-                //                 jenkins_workspace: "${env.WORKSPACE}/",
-                //                 apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}",
-                //                 http_proxy: "${params.http_proxy}"
-                //             ]
-                //         )
-
-                //     } catch (Exception e) {
-                //         echo 'Exception occurred: ' + e.toString()
-                //         sh '''
-                //         echo "Exception Handled"
-                //         cd ${WORKSPACE}/
-                //         '''
-                //         ansiblePlaybook(
-                //             playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
-                //             inventory: "${env.WORKSPACE}/inventory.ini",
-                //             forks: 16,
-                //             colorized: true,
-                //             extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-                //             extraVars: [
-                //                 jenkins_workspace: "${env.WORKSPACE}/",
-                //                 http_proxy: "${params.http_proxy}"
-                //             ]
-                //         )
-                //     }
-                // }
-
             }
         }
 
@@ -354,78 +302,122 @@ pipeline {
             }
         }
         
-        stage('Running KubeSpray') {
-            steps {         
-                // This is the recommended way of running ansible playbooks/roles from Jennkins
-                retry(2) {
-                    sh '''
-                    cd ${WORKSPACE}/roles/tmp/kubespray/
-                    '''
-                    ansiblePlaybook(
-                        playbook: "${env.WORKSPACE}/roles/tmp/kubespray/cluster.yml",
-                        inventory: "${env.WORKSPACE}/inventory.ini",
-                        forks: 16,
-                        colorized: true,
-                        extras: '-u ${user} --become --become-user=root --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-                        extraVars: [
-                            jenkins_workspace: "${env.WORKSPACE}/",
-                            http_proxy: "${params.http_proxy}",
-                            https_proxy: "${params.https_proxy}",
-                            no_proxy: "${params.no_proxy}",
-                            cluster_name: "${params.cluster_name}",
-                            use_external_load_balancer: "${params.use_external_load_balancer}",
-                            apiserver_loadbalancer_domain_name: "${params.apiserver_loadbalancer_domain_name}",
-                            apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}",
-                            apiserver_loadbalancer_port: "${params.apiserver_loadbalancer_port}",
-                            dashboard_enabled: "${params.dashboard_enabled}",
-                            ingress_nginx_enabled: "${params.ingress_nginx_enabled}",
-                            metrics_server_enabled: "${params.metrics_server_enabled}",
-                            helm_enabled: "${params.helm_enabled}",
-                            cert_manager_enabled: "${params.cert_manager_enabled}",
-                            use_internal_loadbalancer: "${params.use_internal_loadbalancer}",
-                            loadbalancer_apiserver_type: "${params.loadbalancer_apiserver_type}",
-                            use_localhost_as_kubeapi_loadbalancer: "${params.use_localhost_as_kubeapi_loadbalancer}",
-                            kube_network_plugin: "${params.kube_network_plugin}",
-                            container_runtime: "${params.container_runtime}",
-                            local_release_dir: "${params.kubespray_temp_dir}",
-                            kube_service_addresses: "${params.kube_service_addresses}",
-                            kube_pods_subnet: "${params.kube_pods_subnet}"
-                        ]
-                    )
-                }
-                // This also works but doesn't show the colors on the output which at the end could help us find easier error or warnings.
-                // sh '''
-                // cd ${WORKSPACE}/roles/tmp/kubespray/ ; echo -e "\n"
-                // pwd ; echo -e "\n"
-                // source venv/bin/activate ; echo -e "\n\n"
-                // until time ansible-playbook -i ${WORKSPACE}/inventory.ini cluster.yml -u root --become --become-user=root --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" ; do sleep 5 ; done
-                // deactivate ; echo -e "\n"s
-            }
-        }
+        // stage('Running KubeSpray') {
+        //     steps {         
+        //         // This is the recommended way of running ansible playbooks/roles from Jennkins
+        //         retry(2) {
+        //             sh '''
+        //             cd ${WORKSPACE}/roles/tmp/kubespray/
+        //             '''
+        //             ansiblePlaybook(
+        //                 playbook: "${env.WORKSPACE}/roles/tmp/kubespray/cluster.yml",
+        //                 inventory: "${env.WORKSPACE}/inventory.ini",
+        //                 forks: 16,
+        //                 colorized: true,
+        //                 extras: '-u ${user} --become --become-user=root --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
+        //                 extraVars: [
+        //                     jenkins_workspace: "${env.WORKSPACE}/",
+        //                     http_proxy: "${params.http_proxy}",
+        //                     https_proxy: "${params.https_proxy}",
+        //                     no_proxy: "${params.no_proxy}",
+        //                     cluster_name: "${params.cluster_name}",
+        //                     use_external_load_balancer: "${params.use_external_load_balancer}",
+        //                     apiserver_loadbalancer_domain_name: "${params.apiserver_loadbalancer_domain_name}",
+        //                     apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}",
+        //                     apiserver_loadbalancer_port: "${params.apiserver_loadbalancer_port}",
+        //                     dashboard_enabled: "${params.dashboard_enabled}",
+        //                     ingress_nginx_enabled: "${params.ingress_nginx_enabled}",
+        //                     metrics_server_enabled: "${params.metrics_server_enabled}",
+        //                     helm_enabled: "${params.helm_enabled}",
+        //                     cert_manager_enabled: "${params.cert_manager_enabled}",
+        //                     use_internal_loadbalancer: "${params.use_internal_loadbalancer}",
+        //                     loadbalancer_apiserver_type: "${params.loadbalancer_apiserver_type}",
+        //                     use_localhost_as_kubeapi_loadbalancer: "${params.use_localhost_as_kubeapi_loadbalancer}",
+        //                     kube_network_plugin: "${params.kube_network_plugin}",
+        //                     container_runtime: "${params.container_runtime}",
+        //                     local_release_dir: "${params.kubespray_temp_dir}",
+        //                     kube_service_addresses: "${params.kube_service_addresses}",
+        //                     kube_pods_subnet: "${params.kube_pods_subnet}"
+        //                 ]
+        //             )
+        //         }
+        //         // This also works but doesn't show the colors on the output which at the end could help us find easier error or warnings.
+        //         // sh '''
+        //         // cd ${WORKSPACE}/roles/tmp/kubespray/ ; echo -e "\n"
+        //         // pwd ; echo -e "\n"
+        //         // source venv/bin/activate ; echo -e "\n\n"
+        //         // until time ansible-playbook -i ${WORKSPACE}/inventory.ini cluster.yml -u root --become --become-user=root --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" ; do sleep 5 ; done
+        //         // deactivate ; echo -e "\n"s
+        //     }
+        // }
 
-        stage('Installing Addons') {
-            steps {   
-                ansiblePlaybook(
-                        playbook: "${env.WORKSPACE}/roles/tmp/kubespray/cluster.yml",
-                        inventory: "${env.WORKSPACE}/inventory.ini",
-                        forks: 16,
-                        colorized: true,
-                        extras: '--tags apps -u ${user} --become --become-user=root --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-                        extraVars: [
-                            http_proxy: "${params.http_proxy}",
-                            https_proxy: "${params.https_proxy}",
-                            no_proxy: "${params.no_proxy}"
-                        ]
-                    )         
-                // This is the recommended way of running ansible playbooks/roles from Jennkins
-                // retry(2) {
-                //     sh '''
-                //     cd ${WORKSPACE}/roles/tmp/kubespray/
-                //     ansible-playbook -i ${WORKSPACE}/inventory.ini cluster.yml --tags apps -u root --become --become-user=root -f 16 --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" -v
-                //     '''
-                // }
-            }
-        }
+        // stage('Installing Addons') {
+        //     steps {
+        //         sh '''
+        //         cd ${WORKSPACE}/roles/tmp/kubespray/
+        //         ansible-playbook -i ${WORKSPACE}/inventory.ini cluster.yml --tags apps -u root --become --become-user=root -f 16 --extra-vars "http_proxy=${http_proxy} https_proxy=${https_proxy} no_proxy=${no_proxy}" -v
+        //         ''' 
+        //     }
+        // }
+
+        // stage('Uninstalling K8s') {
+        //     when {
+        //         expression { params.uninstall_kubespray == true }
+        //     }
+        //     steps {
+        //         script {
+        //             try {
+        //                 ansiblePlaybook(
+        //                     playbook: "${env.WORKSPACE}/roles/tmp/kubespray/reset.yml",
+        //                     inventory: "${env.WORKSPACE}/inventory.ini",
+        //                     forks: 16,
+        //                     colorized: true,
+        //                     become: true,
+        //                     becomeUser: "root",
+        //                     extras: '-u ${user} --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --flush-cache -v',
+        //                     extraVars: [
+        //                         http_proxy: "${params.http_proxy}",
+        //                         https_proxy: "${params.https_proxy}",
+        //                         no_proxy: "${params.no_proxy}",
+        //                         reset_confirmation: "yes"
+        //                     ]
+        //                 )
+
+        //                 ansiblePlaybook(
+        //                     playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
+        //                     inventory: "${env.WORKSPACE}/inventory.ini",
+        //                     forks: 16,
+        //                     colorized: true,
+        //                     extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
+        //                     extraVars: [
+        //                         jenkins_workspace: "${env.WORKSPACE}/",
+        //                         apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}",
+        //                         http_proxy: "${params.http_proxy}"
+        //                     ]
+        //                 )
+
+        //             } catch (Exception e) {
+        //                 echo 'Exception occurred: ' + e.toString()
+        //                 sh '''
+        //                 echo "Exception Handled"
+        //                 cd ${WORKSPACE}/
+        //                 '''
+        //                 ansiblePlaybook(
+        //                     playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
+        //                     inventory: "${env.WORKSPACE}/inventory.ini",
+        //                     forks: 16,
+        //                     colorized: true,
+        //                     extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
+        //                     extraVars: [
+        //                         jenkins_workspace: "${env.WORKSPACE}/",
+        //                         http_proxy: "${params.http_proxy}"
+        //                     ]
+        //                 )
+        //             }
+        //         }
+
+        //     }
+        // }
     }
   
     post {
