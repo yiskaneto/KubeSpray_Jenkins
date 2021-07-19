484ea73a2da85b1a79973025407e7ee66bfd8e18
escanoru <yitzo@yandex.com>
2021-07-19 12:31:35 -0600

    unused staged removed

--- K8_KubeSpray.groovy
+++ K8_KubeSpray.groovy
@@ -382,65 +382,6 @@ pipeline {
                 // deactivate ; echo -e "\n"s
             }
         }
-
-        // stage('Uninstalling K8s') {
-        //     when {
-        //         expression { params.uninstall_kubespray == true }
-        //     }
-        //     steps {
-        //         script {
-        //             try {
-        //                 ansiblePlaybook(
-        //                     playbook: "${env.WORKSPACE}/roles/tmp/kubespray/reset.yml",
-        //                     inventory: "${env.WORKSPACE}/inventory.ini",
-        //                     forks: 16,
-        //                     colorized: true,
-        //                     become: true,
-        //                     becomeUser: "root",
-        //                     extras: '-u ${user} --ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --flush-cache -v',
-        //                     extraVars: [
-        //                         http_proxy: "${params.http_proxy}",
-        //                         https_proxy: "${params.https_proxy}",
-        //                         no_proxy: "${params.no_proxy}",
-        //                         reset_confirmation: "yes"
-        //                     ]
-        //                 )
-
-        //                 ansiblePlaybook(
-        //                     playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
-        //                     inventory: "${env.WORKSPACE}/inventory.ini",
-        //                     forks: 16,
-        //                     colorized: true,
-        //                     extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-        //                     extraVars: [
-        //                         jenkins_workspace: "${env.WORKSPACE}/",
-        //                         apiserver_loadbalancer_address: "${params.apiserver_loadbalancer_address}",
-        //                         http_proxy: "${params.http_proxy}"
-        //                     ]
-        //                 )
-
-        //             } catch (Exception e) {
-        //                 echo 'Exception occurred: ' + e.toString()
-        //                 sh '''
-        //                 echo "Exception Handled"
-        //                 cd ${WORKSPACE}/
-        //                 '''
-        //                 ansiblePlaybook(
-        //                     playbook: "${env.WORKSPACE}/roles/Requirements/main.yaml",
-        //                     inventory: "${env.WORKSPACE}/inventory.ini",
-        //                     forks: 16,
-        //                     colorized: true,
-        //                     extras: '--ssh-extra-args=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -v',
-        //                     extraVars: [
-        //                         jenkins_workspace: "${env.WORKSPACE}/",
-        //                         http_proxy: "${params.http_proxy}"
-        //                     ]
-        //                 )
-        //             }
-        //         }
-
-        //     }
-        // }
     }
   
     post {
