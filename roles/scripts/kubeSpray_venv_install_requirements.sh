fa36f95af56909df8955f07df2a5ac7989f927bf
escanoru <yitzoyi@outlook.com>
2023-02-18 06:49:41 +0000

    update

--- roles/scripts/kubeSpray_venv_install_requirements.sh
+++ roles/scripts/kubeSpray_venv_install_requirements.sh
@@ -13,8 +13,11 @@ source kubespray-venv/bin/activate ; which python ; echo -e "\n\n"
 echo -e "Verifying current directory"
 pwd ; echo -e "\n\n"
 
-echo -e "Running python install -r requirements.txt\n"
-test -f requirements-$ANSIBLE_VERSION.txt && ansible-galaxy role install -r requirements-$ANSIBLE_VERSION.txt && ansible-galaxy collection -r requirements-$ANSIBLE_VERSION.txt
+echo -e "Running python -m pip install -r requirements-$ANSIBLE_VERSION.txt\n"
+python -m pip install -r requirements-$ANSIBLE_VERSION.txt
+
+# echo -e "Running ansible-galazy agains the \n"
+# test -f requirements-$ANSIBLE_VERSION.yml && ansible-galaxy role install -r requirements-$ANSIBLE_VERSION.yml && ansible-galaxy collection -r requirements-$ANSIBLE_VERSION.yml
 
 echo -e "Verifying current directory"
 pwd ; echo -e "\n"
